400c2a30590cacc44522dc65bc0a6e14290aee6b
carlomnop <cuevascarlo07@gmail.com>
2017-01-24 17:39:10 +0800

    added and modified borrow reports

--- build/ajax/insertBorrowHistory.php
+++ build/ajax/insertBorrowHistory.php
@@ -14,16 +14,14 @@
 	$no_format = str_pad($no, 5, '0', STR_PAD_LEFT); //format to 5 digit
 
 	$ctrl_no = $CTRL_NO_PREFIX . "-" . $sy . "-" . $no_format . $CTRL_NO_SUFFIX;
-
 	//===============magic happens================//
 	$transferRequestDatas = $db->select("borrow_request", [
 		"request_code", "id", "qty", "condition_id", "old_loc_id", "new_loc_id", "transfer_to", "released_from", "remarks"
 	], [
 		"request_code" => $request_code
 	]);
-
 	foreach ($transferRequestDatas as $transferRequestData) {
-		$db->insert("borrow_request_history", [
+	$insert =	$db->insert("borrow_request_history", [
 			"ctrl_no" => $ctrl_no,
 			"sy" => $sy,
 			"no" => $no,
@@ -33,109 +31,13 @@
 			"condition_id" => $transferRequestData['condition_id'],
 			"old_loc_id" => $transferRequestData['old_loc_id'],
 			"new_loc_id" => $transferRequestData['new_loc_id'],
-			"transfer_to" => $transferRequestData['transfer_to'],
+			"borrowed_to" => $transferRequestData['transfer_to'],
 			"released_from" => $transferRequestData['released_from'],
 			"remarks" => $transferRequestData['remarks'],
-			"date_approved" => $dateToday
+			"date_approved" => $dateToday,
+			"date_returned" => $dateToday
 		]); //insert things to history
-
-		$item_qty = $db->get("property_accountability", "qty", [
-			"AND" => [
-				"property_id" => $transferRequestData['id'],
-				"emp_id" => $transferRequestData['released_from'],
-				"location_id" => $transferRequestData['old_loc_id'],
-				"condition_id" => $transferRequestData['condition_id']
-				]
-			]); //get the quantity of the item where it is accountable originally
-
-		if ($item_qty > $transferRequestData['qty']) { //check if there will still item to be left on his accountability
-
-			if ($db->has("property_accountability", [
-				"AND" => [
-					"property_id" => $transferRequestData['id'],
-					"emp_id" => $transferRequestData['transfer_to'],
-					"location_id" => $transferRequestData['new_loc_id'],
-					"condition_id" => $transferRequestData['condition_id']
-					]
-				])) { //if the employee has already the item
-				$db->update("property_accountability", [
-					"qty[+]" => $transferRequestData['qty']
-					], [
-						"AND" => [
-							"property_id" => $transferRequestData['id'],
-							"emp_id" => $transferRequestData['transfer_to'],
-							"location_id" => $transferRequestData['new_loc_id'],
-							"condition_id" => $transferRequestData['condition_id']
-						]
-					]); //add the quantity
-			} else { //else if the employee doesnt have the item
-				$db->insert("property_accountability", [
-					"emp_id" => $transferRequestData['transfer_to'],
-					"property_id" => $transferRequestData['id'],
-					"qty" => $transferRequestData['qty'],
-					"location_id" => $transferRequestData['new_loc_id'],
-					"condition_id" => $transferRequestData['condition_id'],
-					"remarks" => $transferRequestData['remarks']
-				]); //add item to his accountability
-			}
-
-			$db->update("property_accountability", [
-				"qty[-]" => $transferRequestData['qty']
-				], [
-					"AND" => [
-						"property_id" => $transferRequestData['id'],
-						"emp_id" => $transferRequestData['released_from'],
-						"location_id" => $transferRequestData['old_loc_id'],
-						"condition_id" => $transferRequestData['condition_id']
-					]
-				]); //deduct the quantity from the orig owner
-
-		} else { //if that was the last item, change the accountability to requestor
-
-			//check if the employee has the item
-			if ($db->has("property_accountability", [
-				"AND" => [
-					"property_id" => $transferRequestData['id'],
-					"emp_id" => $transferRequestData['transfer_to'],
-					"location_id" => $transferRequestData['new_loc_id'],
-					"condition_id" => $transferRequestData['condition_id']
-					]
-				])) { //if the employee has already the item
-					$db->update("property_accountability", [
-						"qty[+]" => $transferRequestData['qty']
-						], [
-							"AND" => [
-								"property_id" => $transferRequestData['id'],
-								"emp_id" => $transferRequestData['transfer_to'],
-								"location_id" => $transferRequestData['new_loc_id'],
-								"condition_id" => $transferRequestData['condition_id']
-							]
-						]); //add the quantity
-
-					$db->delete("property_accountability", [
-						"AND" => [
-							"property_id" => $transferRequestData['id'],
-							"emp_id" => $transferRequestData['released_from'], //requestor
-							"location_id" => $transferRequestData['old_loc_id'],
-							"condition_id" => $transferRequestData['condition_id']
-						]
-					]); //delete it because it goes all to the new owner
-				} else { //else if the employee doesnt have the item
-					$db->update("property_accountability", [
-						"emp_id" => $transferRequestData['transfer_to'],
-						"location_id" => $transferRequestData['new_loc_id']
-					], [
-						"AND" => [
-							"property_id" => $transferRequestData['id'],
-							"emp_id" => $transferRequestData['released_from'], //requestor
-							"location_id" => $transferRequestData['old_loc_id'],
-							"condition_id" => $transferRequestData['condition_id']
-						]
-					]); //update the accountable, change ownership
-				}
-		}
 	}
-
 	$db->delete("borrow_request", ["request_code" => $request_code]); //lastly, delete it from transfer request
 	echo 1;
 ?>
--- build/ajax/showHistoryBorrowRequest.php
+++ build/ajax/showHistoryBorrowRequest.php
@@ -6,7 +6,7 @@
 		exit();
 	}
 
-  $sql = "SELECT ctrl_no, SUM(id) AS items_borrowed, CONCAT(b.last_name, ', ', b.first_name) AS borrowed_to, CONCAT(c.last_name, ', ', c.first_name) AS released_from, date_approved FROM borrow_request_history AS a LEFT JOIN account_table AS b ON a.borrowed_to=b.emp_id LEFT JOIN account_table AS c ON a.borrowed_to=c.emp_id GROUP BY ctrl_no";
+  $sql = "SELECT ctrl_no, qty, CONCAT(b.last_name, ', ', b.first_name) AS borrowed_to, CONCAT(c.last_name, ', ', c.first_name) AS released_from, date_approved FROM borrow_request_history AS a LEFT JOIN account_table AS b ON a.borrowed_to=b.emp_id LEFT JOIN account_table AS c ON a.borrowed_to=c.emp_id GROUP BY ctrl_no";
 
   $transferHistoryDatas = $db->query($sql)->fetchAll();
   $historyCount = count($transferHistoryDatas);
@@ -18,7 +18,7 @@
       <tr>
         <td class="sortable-column"></td>
         <td class="sortable-column">Control Number</td>
-        <td class="sortable-column">Item Transferred</td>
+        <td class="sortable-column">Item Borrowed</td>
         <td class="sortable-column">Transfer To</td>
         <td class="sortable-column">Released From</td>
         <td class="sortable-column">Date Approved</td>
@@ -30,9 +30,9 @@
       {
       ?>
       <tr>
-        <td><div class="toolbar"><button class="toolbar-button button primary"onclick="window.open('build/reports/transferReport.php?ctrl_no=<?php echo $transferHistoryData['ctrl_no']?>', '', '');"><span class="mif-print icon"></span></button></div></td>
+        <td><div class="toolbar"><button class="toolbar-button button primary"onclick="window.open('build/reports/borrowReport.php?ctrl_no=<?php echo $transferHistoryData['ctrl_no']?>', '', '');"><span class="mif-print icon"></span></button></div></td>
         <td><?php echo $transferHistoryData['ctrl_no']?> </td>
-        <td><?php echo $transferHistoryData['items_borrowed']?> </td>
+        <td><?php echo $transferHistoryData['qty']?> </td>
         <td><?php echo $transferHistoryData['borrowed_to']?> </td>
         <td><?php echo $transferHistoryData['released_from']?> </td>
         <td><?php echo date('M d, Y H:i:A', strtotime($transferHistoryData['date_approved'])); ?> </td>
