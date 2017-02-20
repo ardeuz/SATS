c0f0243969cb2fb751e14f83f4af5e6e97d497e2
carlomnop <cuevascarlo07@gmail.com>
2017-02-20 16:02:41 +0800

    added server side for repair

---
+++ build/server_side/adminServerRepair.php
@@ -0,0 +1,20 @@
+<?php
+  require( '../dataTables/ssp.php' );
+  $table = "audit_trail_condition";
+  $pkey = "date";
+  $column = array(
+    array('db' => '`u`.`action`', 'dt' => 0,'field'=>"action"),
+    array('db' => '`u`.`date`', 'dt' => 1,'field' => "date")
+  );
+  $sql_details = array(
+  	'user' => "root",
+  	'pass' => "",
+  	'db'   => "sats",
+  	'host' => "localhost"
+  );
+  $joinQuery = "FROM `audit_trail_condition` AS `u`";
+  echo json_encode(
+    SSP::simple( $_GET, $sql_details, $table, $pkey, $column, $joinQuery )
+  );
+  return;
+?>
