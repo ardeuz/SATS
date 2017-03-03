37baa4b5ab116e4989105fb55f6bdde3ebf839e1
carlomnop <cuevascarlo07@gmail.com>
2017-03-03 18:03:44 +0800

    updated files

--- build/ajax/showSelectors.php
+++ build/ajax/showSelectors.php
@@ -2,7 +2,7 @@
 
   require_once('../../connection.php');
   if(isset($_POST['showSelect'])){
-  $selectorDatas = $db->select("showselectors",["id","pcode","description"]);
+  $selectorDatas = $db->select("showselectors",["id","pcode","description"],["minor_category[<>]"=>[2,6]]);
   foreach($selectorDatas as $selectorData){
     echo '<option value='.$selectorData['id'].'>'.htmlspecialchars($selectorData['pcode']).' - '.$selectorData['description'].'</option>';
   }
--- build/ajax/showSelectorsParent.php
+++ build/ajax/showSelectorsParent.php
@@ -2,7 +2,7 @@
 
   require_once('../../connection.php');
   if(isset($_POST['showSelect'])){
-  $selectorDatas = $db->select("showselectors",["id","pcode","description"]);
+  $selectorDatas = $db->select("showselectors",["id","pcode","description"],["minor_category"=>1]);
   foreach($selectorDatas as $selectorData){
     echo '<option value='.$selectorData['id'].'>'.htmlspecialchars($selectorData['pcode']).' - '.$selectorData['description'].'</option>';
   }
